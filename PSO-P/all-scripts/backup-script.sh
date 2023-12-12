9891cec0c4ac1ad49a2105c1934a6780a104f1e7
alexenache10 <127024681+alexenache10@users.noreply.github.com>
2023-12-12 17:48:36 +0200

    Add files via upload

--- PSO-P/all-scripts/backup-script.sh
+++ PSO-P/all-scripts/backup-script.sh
@@ -4,11 +4,12 @@ if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
 log_file="/home/$user/PSO-P/log.txt"
 mount_folder="/home/$user/PSO-P/mount-folder"
 backup_folder="/home/$user/PSO-P/backup-folder"
-mount_device="/dev/sdb1"
+mount_device="/dev/$(lsblk -o NAME,TYPE,MOUNTPOINT | grep 'disk\|part' | tail -n 1 | cut -c 7-10
+)"
 
 
 echo "STARTING BACKUP..." >> "$log_file"
-mount "$mount_device" "$mount_folder"
+mount "$mount_device" "$mount_folder" 
 sleep 2
 rsync -az "$mount_folder/" "$backup_folder" && umount "$mount_device"
 echo "BACKUP ENDED SUCCESFULLY!" >> "$log_file"
